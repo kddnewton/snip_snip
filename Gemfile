f3c38351b022a74aad700bcf5c6539deb76f0a8e
Kevin Deisz <kevin.deisz@gmail.com>
2018-02-19 11:11:29 -0500

    Test against Rails 5.2

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gemspec
 
-gem 'rails', '~> 5.1'
+gem 'rails', '~> 5.2.0.rc1'
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -11,6 +11,9 @@ require 'snip_snip'
 
 module Dummy
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration should go into files in config/initializers
